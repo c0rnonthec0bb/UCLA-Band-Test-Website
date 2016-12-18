1cc35582e3905d76028146501852c23b7e4e90f3
Adam Cobb <cobb.2326@gmail.com>
2016-12-18 03:10:20 -0800

    organized top elements in ‘top.php’ file

---
+++ wp-content/themes/UCLA Band Theme/top.php
@@ -0,0 +1,45 @@
+
+<div align="right">
+    <table class="social">
+        <tr>
+            <td>
+                <a href="https://www.youtube.com/user/UCLABandVideoProject" target="_blank">
+                    <img src="<?php bloginfo('template_directory');?>/images/icon-youtube.png">
+                </a>
+            </td>
+            <td>
+                <a href="https://www.facebook.com/uclabruinband/" target="_blank">
+                    <img src="<?php bloginfo('template_directory');?>/images/icon-facebook.png" >
+                </a>
+            </td>
+            <td>
+                <a href="https://www.instagram.com/uclabruinmarchingband/" target="_blank">
+                    <img src="<?php bloginfo('template_directory');?>/images/icon-instagram.png">
+                </a>
+            </td>
+            <td>
+                <a href="https://twitter.com/UCLABruinBand/" target="_blank">
+                    <img src="<?php bloginfo('template_directory');?>/images/icon-twitter.png">
+                </a>
+            </td>
+        </tr>
+    </table>
+</div>
+
+<div align="center" style="margin-top: -3vw; margin-bottom: 4vmin;">
+    <div style="width: 80vmin;">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" style="text-decoration:none;">
+            <table style="margin:0;">
+                <tr>
+                    <td>
+                        <img src="<?php bloginfo('template_directory');?>/images/logo-gold.png" style="width: 16vmin;" >
+                    </td>
+                    <td align="right">
+                        <p class="top title" style="font-size: 8vmin;">Marching Band</p>
+                        <p class="top flourish" style="font-size: 3.8vmin">"The Solid Gold Sound"</p>
+                    </td>
+                </tr>
+            </table>
+        </a>
+    </div>
+</div>
