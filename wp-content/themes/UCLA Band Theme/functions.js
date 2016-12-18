94cf9003483f6b679334ab236f02292bf2332ed3
Adam Cobb <cobb.2326@gmail.com>
2016-12-18 01:20:12 -0800

    Updated home page with temporary link to general info page

--- wp-content/themes/UCLA Band Theme/functions.js
+++ wp-content/themes/UCLA Band Theme/functions.js
@@ -2,7 +2,6 @@
  
  $( document ).ready( function() {
         body = $( document.body );
-        body.removeAttr('background-image');
     }
 )
 
--- wp-content/themes/UCLA Band Theme/homepage.php
+++ wp-content/themes/UCLA Band Theme/homepage.php
@@ -1,20 +1,24 @@
 
 
 <div align="center">
-<table id="home-sliders" class="card" style="width:80vw;" align="center">
-<tr>
-<td class="spacer"></td>
-<td align="center">
-<div class="slider">
-    <?php echo do_shortcode("[R-slider id='1']"); ?>
+    <table id="home-sliders" class="card" style="width:80vw;" align="center">
+        <tr>
+            <td class="spacer"></td>
+            <td align="center">
+                <div class="slider">
+                    <?php echo do_shortcode("[R-slider id='1']"); ?>
+                </div>
+            </td>
+            <td align="center">
+                <div class="slider" style="margin-left: 1vmin;">
+                    <?php echo do_shortcode("[R-slider id='3']");?>
+                </div>
+            </td>
+            <td class="spacer"></td>
+        </tr>
+    </table>
 </div>
-</td>
-<td align="center">
-<div class="slider" style="margin-left: 1vmin;">
-    <?php echo do_shortcode("[R-slider id='3']");?>
-</div>
-</td>
-<td class="spacer"></td>
-</tr>
-</table>
+
+<div align="center" style="padding-top:20pt">
+<a class="title top" href="<?php echo esc_url( home_url( '/' ) ); ?>about/general-info/" >Click here to go to the General Info page (temporary)</a>
 </div>
